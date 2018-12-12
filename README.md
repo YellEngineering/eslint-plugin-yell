f528ee53a8b38a8e291deed981ce8cd30dc7bc5d
Arshdeep Soni <arshdeep.soni@yell.com>
2018-12-12 11:59:57 +0000

    updated custom yell plugins to accept parameters

--- README.md
+++ README.md
@@ -5,9 +5,27 @@ These are Yell's Custom ESLint rules used throughout engineering.
 
 
 ---
-## Define Babel ES6
+## AMD Force Parameter
 
-Rule Name: **defineBabelES6**
+Rule Name: **amdForceParameter**
+- Custom YELL plugin to enforce paramaters in define Array and Function callback
+
+### Usage
+```js
+/**
+ * @param files: {Array} : Extensions of files to process
+ * @param params: { Array } : Parameters to look for in Array and Function
+ * @param checkInArray: { Boolean } Check in Array?
+ * @param checkInFunction: { Boolean } Check in Function?
+ */
+"yell/amdForceParameter": [2, {
+	files: ['.es6'],
+	params: ['babelHelpers'],
+	checkInArray: true,
+	checkInFunction: true
+}],
+
+```
 
 ```
 // VALID!
@@ -28,3 +46,21 @@ define(['$'], function($) { })
 ```
 
 ---
+
+## AMD Path Checker
+
+Rule Name: **amdPathChecker**
+- Custom YELL plugin which checks for particular substring in paths
+
+```js
+/**
+ * @param checkFor: { String } check for specific substring in parameters
+ * @param forceSlashInPath: { Boolean } Also checks paramter for '/'
+ * @param excludePaths: { Array } Exclude specific substrings
+ */
+"yell/amdPathChecker": [2, {
+	checkFor: '.es6',
+	forceSlashInPath: true,
+	excludePaths: ['babelHelpers', 'libs/', 'stubs/', 'tests/', '.css', '.html', '.vue', '.js'],
+}]
+```
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
 	rules: {
-		defineBabelES6: {
+		amdForceParameter: {
 			meta: {
 				docs: {
 					description: 'Always require babelHelpers in define statement for ES6 files',
@@ -8,19 +8,34 @@ module.exports = {
 					recommended: true
 				}
 			},
+			schema: [
+				{
+					type: "object",
+					properties: {
+						params: {
+							type: "array"
+						},
+						checkInArray: true,
+						checkInFunction: true
+					}
+				}
+			],
 			create(context) {
 				return {
 					Identifier(node) {
 
+						const options = context.options[0];
+						const params = options['params'];
+
 						/**
 						 * stop if not an es6 file
 						 */
-						let isES6File = /(.*).es6$/.test(context.getFilename());
-						if (!isES6File) {
+
+						let filesToCheck = options['files'].some(item => context.getFilename().includes(item));
+
+						if (!filesToCheck) {
 							return;
 						}
-
-						const SEARCHTERM = 'babelHelpers';
 
 						// check if node is define?
 						const isDefine = node.name.toLowerCase() === 'define';
@@ -43,31 +58,109 @@ module.exports = {
 						const FunctionExpression = args.find(item => item.type === "FunctionExpression");
 						const ArrowFunctionExpression = args.find(item => item.type === "ArrowFunctionExpression");
 
-						// determine if any of them contain babelHelpers
-						const FunctionExpressionContainsBabel = (FunctionExpression && FunctionExpression.params.find(item => item.name === SEARCHTERM));
-						const ArrowFunctionContainsBabel = (ArrowFunctionExpression && ArrowFunctionExpression.params.find(item => item.name === SEARCHTERM));
 
-						// check ifSEARCHTERMexists as a value in array
-						const babelExistsInArray = ArrayExpression && ArrayExpression.elements.find(item => item.value === SEARCHTERM);
+						if ( options['checkInFunction'] ) {
+							const FunctionExpressionContainsParams = FunctionExpression && FunctionExpression.params.find(item => params.some(param => item.name === param));
+							const ArrowFunctionContainsParams = ArrowFunctionExpression && ArrowFunctionExpression.params.find(item => params.some(param => item.name === param));
 
-						// check if babelHelpers exists as a parameter in function callback
-						const babelExistsInFunction = FunctionExpressionContainsBabel || ArrowFunctionContainsBabel;
+							const paramExistsInFunction = FunctionExpressionContainsParams || ArrowFunctionContainsParams;
 
-						// create display message
-						let message = '';
-						if (!babelExistsInArray) {
-							message = 'babelHelpers missing in define Array'
-						} else if (!babelExistsInFunction) {
-							message = 'babelHelpers missing in define function callback'
+							if ( !paramExistsInFunction ) {
+								context.report({
+									node,
+									message: 'babelHelpers missing in Define Function Callback'
+								});
+							}
 						}
 
-						// if message is not empty lint as error!
-						if (message) {
+
+						if ( options['checkInArray'] ) {
+							const ArrayExpressionContainsParam = ArrayExpression && ArrayExpression.elements.find(item => params.some(param => item.value === param));
+							if ( !ArrayExpressionContainsParam ) {
+								context.report({
+									node,
+									message: 'babelHelpers missing in Define Array'
+								});
+							}
+						}
+
+
+
+						// // determine if any of them contain babelHelpers
+						// const FunctionExpressionContainsBabel = (FunctionExpression && FunctionExpression.params.find(item => item.name === SEARCHTERM));
+						// const ArrowFunctionContainsBabel = (ArrowFunctionExpression && ArrowFunctionExpression.params.find(item => item.name === SEARCHTERM));
+
+						// // check ifSEARCHTERMexists as a value in array
+						// const babelExistsInArray = ArrayExpression && ArrayExpression.elements.find(item => item.value === SEARCHTERM);
+
+						// // check if babelHelpers exists as a parameter in function callback
+						// const babelExistsInFunction = FunctionExpressionContainsBabel || ArrowFunctionContainsBabel;
+					}
+				};
+			}
+		},
+
+		amdPathChecker: {
+			meta: {
+				docs: {
+					description: 'Ensure that references to files in DEFINE or REQUIRE have .es6 extension if they contain /',
+					category: 'Yell - JS Best Practices',
+					recommended: false
+				}
+			},
+
+			schema: [
+				{
+					type: "object",
+					properties: {
+						excludePaths: {
+							type: "array"
+						},
+					}
+				}
+			],
+
+			create(context) {
+				return {
+					Identifier(node) {
+						const options = context.options[0];
+						const excludePaths = options['excludePaths'];
+						const pathToCheckFor = options['checkFor'];
+						const forceSlash = options['forceSlashInPath'];
+
+						// check if node is define?
+						const isDefineOrRequire = node.name.toLowerCase() === 'define' || node.name.toLowerCase() === 'require';
+						const isCallExpression = node.parent.type === 'CallExpression';
+
+						const isAMDNode = (isDefineOrRequire && isCallExpression);
+
+						// stop if not a define node
+						if (!isAMDNode) {
+							return;
+						}
+
+						// get all the arguments of the define node
+						const args = node.parent.arguments;
+
+						// get array in define
+						const ArrayExpression = args.find(item => item.type === "ArrayExpression");
+
+						// get the element
+						const elementIsInvalid = ArrayExpression && ArrayExpression.elements && ArrayExpression.elements.find(item => {
+							if ( item && item.value ) {
+								const isExclusion = excludePaths.some(toExclude => item.value.includes(toExclude));
+								return !isExclusion && (forceSlash ? item.value.includes('/') : true) && !item.value.includes(pathToCheckFor);
+							}
+							return null;
+						});
+
+						if ( elementIsInvalid ) {
 							context.report({
 								node,
-								message
+								message: `Looks like ${elementIsInvalid.value} should have ${pathToCheckFor} extension`
 							});
 						}
+
 					}
 				};
 			}
